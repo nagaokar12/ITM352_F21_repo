9a10a7734c9bdb7a7ca45b832d81a7565077d498
nagaokar12 <nagaokar@hawaii.edu>
2021-09-21 11:54:07 -1000

    Uploaded Lab 6

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age =  21;
+var fav_num = 12;
+var day_of_birth = 28;
+var month_of_birth = 8;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log( (age + fav_num) / day_of_birth * month_of_birth);
