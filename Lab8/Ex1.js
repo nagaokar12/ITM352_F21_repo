68b92cdbba091d31d8827a8c6afeac737cbcd846
nagaokar12 <nagaokar@hawaii.edu>
2021-10-05 11:46:39 -1000

    Uploaded class work from 10/5

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,8 +1,10 @@
 require("./product_data.js");
 var num_products = 5;
 var count = 1;
-while(count <= num_products) {
-    console.log(count);
+/* While count is less than equal to num_products (count up to value of num_products) */
+while(count <= (num_products/2)) { 
+    console.log(`${count}. ${eval('name' + count)}`);
     /* Increment count */
     count++;
 }
+console.log("That's all we have!");
