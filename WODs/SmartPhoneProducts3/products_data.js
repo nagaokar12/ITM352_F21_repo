2052eb5f0fa3c01fb1aa94b7e46f5d3a08d3ee01
nagaokar12 <nagaokar@hawaii.edu>
2021-10-13 11:02:34 -1000

    Uploaded SmartPhoneProducts3

---
+++ WODs/SmartPhoneProducts3/products_data.js
@@ -0,0 +1,27 @@
+    var product1 = {};
+    product1.brand = "HTC";
+    product1.price = 40.00;
+    product1.image = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+    var product2 = {};
+    product2.brand = "Apple";
+    product2.price = 75.00;
+    product2.image = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+    var product3 = {};
+    product3.brand = "Nokia";
+    product3.price = 35.00;
+    product3.image = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+    var product4 = {};
+    product4.brand = "Samsung";
+    product4.price = 45.00;
+    product4.image = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+    var product5 = {};
+    product5.brand = "Blackberry";
+    product5.price = 10.00;
+    product5.image = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+
+    /* Product array */
+    products = [product1, product2, product3, product4, product5];
