04d5d32e833f927eede5494e4aadcd5a33f11f14
nagaokar12 <nagaokar@hawaii.edu>
2021-11-09 11:48:47 -1000

    Class work from 11/9

--- Lab13/info_server_Ex2a.js
+++ Lab13/info_server_Ex2a.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path);
+    console.log(request.method + ' to path ' + request.path + ' query string ' + JSON.stringify(request.query));
+    next();
 });
+
+app.get('/test', function (request, response, next) {
+    response.send('in GET /test');
+});
+
+app.use(express.static('./public'));
+
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
