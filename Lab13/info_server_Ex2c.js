d0ea90469eaa7db1f7b06f9fbeaa0cead485512f
nagaokar12 <nagaokar@hawaii.edu>
2021-11-16 01:38:34 -1000

    Uploaded Lab13 work

--- Lab13/info_server_Ex2c.js
+++ Lab13/info_server_Ex2c.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+
+
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path + ' query string ' + JSON.stringify(request.query));
     next();
@@ -8,5 +10,7 @@ app.all('*', function (request, response, next) {
 app.get('/test', function (request, response, next) {
     response.send('in GET /test');
 });
+
+app.use(express.static('./public'));
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
---
+++ Lab13/info_server_Ex3.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path + ' query string ' + JSON.stringify(request.query));
+    next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.post("/process_form", function (request, response) {
+    var q = request.body['quantity_textbox'];
+    if (typeof q != 'undefined') {
+        response.send(`Thank you for purchasing ${q} things!`);
+    }
+    else {
+        response.send(`Invalid quantity. Press the back button and try again.`);
+    }
+});
+
+app.get('/test', function (request, response, next) {
+    response.send('in GET /test');
+});
+
+app.use(express.static('./public'));
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+
+function isNonNegInt(q, returnErrors = false) {
+    /* Check if a string q is a non-neg integer. If returnErrors is true, the array of errors is returned. Others returns true if q is a non-neg int. */
+    errors = []; // assume no errors at first
+    if (q == '') q = 0;
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    else {
+        if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+        if (parseInt(q) != q) errors.push(' Not an integer!'); // Check that it is an integer
+    }
+    return returnErrors ? errors : (errors.length = 0);
+}
