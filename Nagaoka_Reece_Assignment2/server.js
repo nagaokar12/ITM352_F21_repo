e1cbe3ded235d840735d096c6dcbb576b37aee7b
nagaokar12 <nagaokar@hawaii.edu>
2021-12-01 19:38:57 -1000

    Update Assignment2

--- Nagaoka_Reece_Assignment2/server.js
+++ Nagaoka_Reece_Assignment2/server.js
@@ -5,9 +5,10 @@
  */
 
 /* Based on server.js from Assignment 1 from Momoka Michimoto, FALL 2021 and modifed since */
+/* Also received help from Prof. Port */
 
 /* Require link to product data file */
-var products = require('./products.json');
+var products = require(__dirname + '/products.json');
 
 /* Set the initial amount in inventory */
 products.forEach((prod, i) => { prod.quantity_available = 30; });
@@ -16,25 +17,133 @@ var app = express();
 
 /* Initialize QueryString package */
 const qs = require('querystring');
-const { truncate } = require('fs');
+var fs = require('fs');
+const { URLSearchParams } = require('url');
+
+/* Store user information */
+var filename = __dirname + '/user_data.json';
+
+if (fs.existsSync(filename)) {
+    /* Read filename (from my Lab 14 Ex1b.js) */
+    var user_info = fs.readFileSync(filename, 'utf-8');
+    var user_data = JSON.parse(user_info);
+}
+else {
+    console.log(filename + ' does not exist.');
+}
 
 /*  Monitor all requests */
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
 
     /* Continue */
-    next(); 
+    next();
 });
 
 /* Get body */
 app.use(express.urlencoded({ extended: true }));
 
+/* Taken from my Lab 14 Ex4.js and modified */
+/* --- For the login page --- */
+app.post("/process_login", function (request, response) {
+    var error;
+
+    /* Process login form POST and redirect to logged in page if ok, back to login page if not */
+    /* Make it so capitalization is irrelevant for usernames */
+    let login_username = request.body['username'].toLowerCase();
+    let login_password = request.body['password'];
+
+    /* Check if username exists */
+    if (typeof user_data[login_username] != 'undefined') {
+        /* Then checks password entered matches stored password */
+        if (user_data[login_username].password == login_password) {
+            /* Redirects to the invoice page and displays items purchased */
+            request.query['username'] = login_username;
+            request.query['email'] = user_data[login_username].email;
+            response.redirect('./invoice.html?' + qs.stringify(request.query));
+            return;
+        }
+        else {
+            /* If password is incorrect */
+            error = 'Incorrect password';
+        }
+    }
+    else {
+        /* If username has not been created */
+        error = `${login_username} does not exist`;
+    }
+
+    /* If there are errors, send back to login page with errors */
+    request.query['username'] = login_username;
+    request.query['error'] = error;
+    response.redirect(`./login.html?` + qs.stringify(request.query));
+});
+
+/* Also taken from my Lab 14 Ex.4.js and modified */
+/* --- For the register page --- */
+app.post("/register", function (request, response) {
+    var new_errors = {};
+
+    /* process a simple register form */
+    /* Make it so capitalization is irrelevant for usernames */
+    var new_username = request.body['username'].toLowerCase();
+
+    /* Require only letters to be used for usernames */
+    if (/^[A-Za-z]{4,10}$/.test(request.body.username) == false) {
+       new_errors['username'] = 'You must only put letters in your username. '
+    }
+
+    /* Require a specific email format */
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(request.body.email) == false) {
+        new_errors['email'] = 'Please enter a valid email address'
+    }
+
+    /* Require a unique username */
+    if (typeof user_data[new_username] != 'undefined') {
+        new_errors['username'] = 'Username is already taken.'
+    }
+
+    /* Require a minimum of 4 characters and no more than 10 */
+    if(request.body.password.length < 6) {
+        new_errors['password'] = 'You must enter a minimum of 6 characters.'
+    }
+
+    /* Confirm that both passwords were entered correctly */
+    if(request.body.password !== request.body.repeat_password) {
+        new_errors['repeat_password'] = 'Both passwords must match'
+    }
+
+    /* If new_errors is empty */
+    if (JSON.stringify(new_errors) == '{}') {
+        /* Write data and send to invoice.html */
+        user_data[new_username] = {};
+        user_data[new_username].password = request.body.password;
+        user_data[new_username].email = request.body.email;
+        fs.writeFileSync(filename, JSON.stringify(user_data));
+
+        /* Add username and email to query */
+        request.query['username'] = new_username;
+        request.query['email'] = user_data[new_username].email;
+        response.redirect('./invoice.html?' + qs.stringify(request.query));
+        return;
+    }
+    else {
+        /* Put errors and registration data into query */
+        request.query['reg_errors'] = JSON.stringify(new_errors);
+        request.query['reg_data'] = JSON.stringify(request.body);
+        response.redirect(`./register.html?` + qs.stringify(request.query));
+    }
+});
+
 /* Get quantity data from order form and check it */
+/* ----- Process form ----- */
 app.post('/process_form', function (request, response) {
     var quantities = request.body["quantity"];
+
     /* Assume no errors or quantities for now */
     var errors = {};
     var check_quantities = false;
+
     /* Check quantities are non-negative integers */
     for (i in quantities) {
         /* Check quantity */
@@ -50,6 +159,7 @@ app.post('/process_form', function (request, response) {
             errors['available_' + i] = `We don't have ${(quantities[i])} ${products[i].model} available.`;
         }
     }
+
     /* Check to see if quantity is selected */
     if (!check_quantities) {
         errors['no_quantities'] = `Please select some items!`;
@@ -75,49 +185,6 @@ app.post('/process_form', function (request, response) {
 
 });
 
-/* Taken from Lab 14 Ex4.js and modified */
-/* For the register page */
-app.post("/register", function (request, response) {
-    // process a simple register form
-    username = request.body.username;
-    if (typeof user_registration_info[username] == 'undefined' && (request.body['password'] == request.body['repeat_password'])) {
-        user_registration_info[username] = {};
-        user_registration_info[username].password = request.body.password;
-        user_registration_info[username].email = request.body.email;
-
-
-        fs.writeFileSync(filename, JSON.stringify(user_registration_info));
-        response.redirect('./login');
-        console.log("Registered!");
-    }
-    else {
-        response.redirect('./register');
-        console.log("Not registered.");
-    }
-});
-
-/* For the login page */
-app.post("/login", function (request, response) {
-    /* Process login form POST and redirect to logged in page if ok, back to login page if not */
-    let login_username = request.body['username'];
-    let login_password = request.body['password'];
-
-    /* Check if username exists, then check password entered matches stored password */
-    if (typeof user_registration_info[login_username] != 'undefined') {
-        if (typeof user_registration_info[login_username]["password"] == login_password) {
-            response.send(`${login_username} is logged in`);
-        }
-        else {
-            response.redirect('./login');
-        }
-    }
-    else {
-        response.send(`${login_username} does not exist`);
-    }
-    response.send('processing login', JSON.stringify(request.body));
-});
-
-
 /* Routing */
 app.get("/products.js", function (request, response, next) {
     response.type('.js');
@@ -126,7 +193,7 @@ app.get("/products.js", function (request, response, next) {
 });
 
 /* Route all other GET requests to files in public */
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 
 /* Start server */
 app.listen(8080, () => console.log(`listening on port 8080`));
